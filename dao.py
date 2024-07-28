4a417f3f1f01f598f279b847a39d6c789c09ab54
Lorenzo Bertetto <lorenzo.bertetto@gmail.com>
2024-07-28 19:56:59 +0200

    Post-meeting

---
+++ dao.py
@@ -0,0 +1,30 @@
+from database import db
+
+"""
+query = [
+    {
+        "collection": "Models",
+        "project": [
+            "name",
+            "openSource"
+        ],
+        "filters": {
+            "name": "ll",
+            "openSource": True
+        }
+    },
+    {
+        "collection", "Downstream Tasks",
+        "project": [
+            "name",
+        ],
+        "filters": {
+            "name": "RO"
+        }
+    }
+]
+"""
+
+def query(specification):
+    pass
+
