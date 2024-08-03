14e926d5a2800c8fee7e3dd0ffe3712a5a6bdabf
Claudio Savelli <claudiosav2000@gmail.com>
2024-08-03 12:41:15 +0200

    read description
    - solved metrics and description retrieval. Removed all the metrics without a description
    - found out how to retrieve carbon emission data, but list_models is not working idk why =(
    - commented tags.py and added some entries

--- data_analysis/tags.py
+++ data_analysis/tags.py
@@ -18,14 +18,14 @@ tag_license_to_use = ['apache-2.0','mit','openrail','creativeml-openrail-m','oth
 					  'odc-by','epl-2.0','lgpl-lr','cc-by-2.5','eupl-1.1','isc','ncsa','etalab-2.0','cdla-sharing-1.0','postgresql','lppl-1.3c',
 					  'epl-1.0','apple-ascl','ofl-1.1','cdla-permissive-1.0']
 
-# TODO: check if correct
+# TODO: check if correct -> does program languages like rust count as a domain?
 tag_library = ['pytorch','tf','jax','transformers','safetensors','tensorboard','peft','diffusers','gguf','stable-baselines3','onnx',
 			   'sentence-transformers','ml-agents','tf-keras','adapters','setfit','timm','sample-factory','flair','keras','mlx',
 			   'transformers.js','spacy','fastai','espnet','openvino','core-ml','nemo','joblib','rust','bertopic','tf-lite','openclip',
 			   'fasttext','scikit-learn','speechbrain','paddlepaddle','fairseq','graphcore','asteroid','llamafile','allennlp','stanza',
 			   'spanmarker','paddlenlp','habana','pyannote.audio','pythae','unity-sentis']
 
-tag_carbon_emission = ['co2_eq_emissions'] # boolean... TODO: where are the values?
+tag_carbon_emission = ['co2_eq_emissions'] # boolean... TODO: where are the values? -> https://huggingface.co/blog/carbon-emissions-on-the-hub but can't retrieve the models like in the example
 
 TAG_DOWNSTREAM_TASK = ['text-classification','token-classification','table-question-answering','question-answering','zero-shot-classification',
 				   'translation','summarization','feature-extraction','text-generation','text2text-generation','fill-mask','sentence-similarity']
@@ -44,8 +44,8 @@ TAG_DOWNSTREAM_TASK = ['text-classification','token-classification','table-quest
 # 'size_categories:10K<n<100K'
 # tag_size = []
 
-# TODO: check if complete (how?)
-tag_domain = ['art','code','medical','legal','finance','biology','music','chemistry','climate']
+# TODO: check if complete (how?) -> in HF dataset page these are the only available (so retrievable in an automated way)
+tag_domain = ['art','code','medical','legal','finance','biology','music','chemistry','climate','Synthetic']
 
 # 'task_categories:question-answering'
 # TAG_DOWNSTREAM_TASK = [] # see LLM
