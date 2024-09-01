f01d2106399baac9213d0ba2eda665cd8e37f4f4
Lorenzo Bertetto <lorenzo.bertetto@gmail.com>
2024-09-01 11:29:08 +0200

    Removed external IP dependency.

--- gui.py
+++ gui.py
@@ -53,15 +53,15 @@ interactive_image_html = """
 <img src="./app/static/db_model.jpeg" alt="DB Model" usemap="#image-map">
 
 <map name="image-map">
-    <area target="" alt="Large Language Model" title="Large Language Model" href="http://localhost:8501/llm_query" coords="392,249,553,294" shape="rect">
-    <area target="" alt="Metric" title="Metric" href="http://localhost:8501/metric_query" coords="76,255,150,288" shape="rect">
-    <area target="" alt="Downstream Task" title="Downstream Task" href="http://localhost:8501/downstream_task_query" coords="409,45,535,87" shape="rect">
-    <area target="" alt="Dataset" title="Dataset" href="http://localhost:8501/dataset_query" coords="805,254,876,289" shape="rect">
-    <area target="" alt="Assess" title="Assess" href="http://localhost:8501/assess_query" coords="113,125,138,148,113,172,89,150" shape="poly">
-    <area target="" alt="Evaluate" title="Evaluate" href="link6" coords="276,248,302,272,277,295,252,273" shape="poly">
-    <area target="" alt="Train" title="Train" href="http://localhost:8501/train_query" coords="649,248,672,272,650,297,625,272" shape="poly">
-    <area target="" alt="Enable" title="Enable" href="http://localhost:8501/enable_query" coords="840,125,865,150,841,174,818,149" shape="poly">
-    <area target="" alt="Suited For" title="Suited For" href="http://localhost:8501/suited_for_query" coords="473,110,497,136,472,158,449,135" shape="poly">
+    <area target="" alt="Large Language Model" title="Large Language Model" href="llm_query" coords="392,249,553,294" shape="rect">
+    <area target="" alt="Metric" title="Metric" href="metric_query" coords="76,255,150,288" shape="rect">
+    <area target="" alt="Downstream Task" title="Downstream Task" href="downstream_task_query" coords="409,45,535,87" shape="rect">
+    <area target="" alt="Dataset" title="Dataset" href="dataset_query" coords="805,254,876,289" shape="rect">
+    <area target="" alt="Assess" title="Assess" href="assess_query" coords="113,125,138,148,113,172,89,150" shape="poly">
+    <!-- <area target="" alt="Evaluate" title="Evaluate" href="link6" coords="276,248,302,272,277,295,252,273" shape="poly"> -->
+    <area target="" alt="Train" title="Train" href="train_query" coords="649,248,672,272,650,297,625,272" shape="poly">
+    <area target="" alt="Enable" title="Enable" href="enable_query" coords="840,125,865,150,841,174,818,149" shape="poly">
+    <area target="" alt="Suited For" title="Suited For" href="suited_for_query" coords="473,110,497,136,472,158,449,135" shape="poly">
 </map>
 """
 
